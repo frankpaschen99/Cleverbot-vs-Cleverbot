b7e857d68924843008ab8c6aec9e4d932985589f
Frank Paschen <frankpaschen99@gmail.com>
2016-06-03 00:04:00 -0400

    faster, more efficient rewrite

--- server/START_SERVER.bat
+++ server/START_SERVER.bat
@@ -1,1 +1,1 @@
-node index.js
+node --harmony index.js
--- server/index.js
+++ server/index.js
@@ -2,56 +2,53 @@ var Cleverbot = require('cleverbot-node');
 CleverbotA = new Cleverbot;
 CleverbotB = new Cleverbot;
 io = require('socket.io').listen(3000);
-clients = [];
+
+class ConversationHandler {
+	constructor() {
+		this.conversations = [];
+	}
+	add(socket, promptString) {
+		this.conversations.push(new Conversation(socket, promptString));
+	}
+	destroy(socket) {
+		this.conversations = [];
+	}
+}
+class Conversation {
+	constructor(_socket, promptString) {
+		this.socket = _socket;
+		this.promptA(promptString, this.socket, this.promptB, this.promptA);
+	}
+	promptA(message, socket, promptB, promptA, destroyed) {
+		Cleverbot.prepare(function() {
+			CleverbotA.write(message, function(response) {
+				console.log("Cleverbot B: " + response.message);
+				socket.emit('response', "Cleverbot A: " + response.message);
+				promptB(response.message, socket, promptB, promptA);
+			});
+		});
+	}
+	promptB(message, socket, promptB, promptA) {
+		Cleverbot.prepare(function() {
+			CleverbotB.write(message, function(response) {
+				console.log("Cleverbot B: " + response.message);
+				socket.emit('response', "Cleverbot B: " + response.message);
+				promptA(response.message, socket, promptB, promptA);
+			});
+		});
+	}
+}
+var ch = new ConversationHandler();
 
 io.sockets.on('connection', function (socket) {
 	console.log("Client connected: " + socket.id);
-	clients.push(socket);
     socket.on("cleverbot_prompt", function(initialPrompt) {
 		console.log("PROMPT: " + initialPrompt);
-		promptA(initialPrompt, socket);
+		ch.add(socket, initialPrompt);
+		ch.destroy(socket);
     });
 	socket.on('disconnect', function() {
-        var index = clients.indexOf(socket);
-        if (index != -1) {
-            clients.splice(index, 1);
-        }
+		ch.destroy(socket);
 		console.log("Client disconnected: " + socket.id);
     });
 });
-
-function promptA(message, socket) {
-    Cleverbot.prepare(function() {
-        CleverbotA.write(message, function(response) {
-            console.log("Cleverbot A: " + response.message);
-            setTimeout(function() {
-				if (clients.length > 0) {
-					clients.forEach(function(index) {
-						if (socket.id == index.id) {
-							index.emit('response', "Cleverbot A: " + response.message);
-						}
-					});
-				} else return;
-                promptB(response.message, socket);
-            }, 1000);
-        });
-    });
-}
-
-function promptB(message, socket) {
-    Cleverbot.prepare(function() {
-        CleverbotB.write(message, function(response) {
-            console.log("Cleverbot B: " + response.message);
-            setTimeout(function() {
-				if (clients.length > 0) {
-					clients.forEach(function(index) {
-						if (socket.id == index.id) {
-							index.emit('response', "Cleverbot B: " + response.message);
-						}
-					});
-				} else return;
-                promptA(response.message, socket);
-            }, 1000);
-        });
-    });
-}
